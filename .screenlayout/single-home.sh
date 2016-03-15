04de4875ed0b07ae8b0aad239e2e74304441ad98
w-vi <vilibald@wvi.cz>
2016-03-15 22:00:06 +0100

    one more screen layout at home

---
+++ .screenlayout/single-home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --primary --mode 1680x1050 --pos 0x0 --rotate normal
