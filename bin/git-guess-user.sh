eeb160bef1240bda69555351ea8b67e2bcbd933c
w-vi <vilibald@wvi.cz>
2019-03-13 16:14:46 +0100

    aliases, gitguessuser, mimeapps update

--- bin/git-guess-user.sh
+++ bin/git-guess-user.sh
@@ -12,6 +12,11 @@ if [[ $remote == *github.com:apiaryio* ]]; then
     email=vilibald.wanca@oracle.com
 fi
 
+if [[ $remote == *bitbucket.oci.oraclecorp.com* ]]; then
+    email=vilibald.wanca@oracle.com
+    user=vwanca
+fi
+
 echo "Configuring user.name as $user and user.email as $email"
 git config user.name $user
 git config user.email $email
