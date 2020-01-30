0fb9d91a5869b93f046ab2ebcbdddf5089798156
w-vi <vilibald@wvi.cz>
2020-01-30 09:24:01 +0100

    fluxbox apps and keys and urxvt utf8 font

--- .conky/seamod_rings.lua
+++ .conky/seamod_rings.lua
@@ -208,7 +208,7 @@ gauge = {
     caption_fg_colour=0xFFFFFF,    caption_fg_alpha=0.5,
 },
 {
-    name='downspeedf',           arg='enp5s0',                     max_value=100,
+    name='downspeedf',           arg='eno1',                     max_value=100,
     x=70,                          y=660,
     graph_radius=54,
     graph_thickness=7,
@@ -229,7 +229,7 @@ gauge = {
     caption_fg_colour=0xFFFFFF,    caption_fg_alpha=0.5,
 },
 {
-    name='upspeedf',           arg='enp5s0',                     max_value=100,
+    name='upspeedf',           arg='eno1',                     max_value=100,
     x=70,                          y=660,
     graph_radius=42,
     graph_thickness=7,
